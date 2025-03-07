30c4e85fe27bc1e40d7a57bcfb436f0e8543e108
siyakholwa-bonga <129983262+siyakholwa-bonga@users.noreply.github.com>
2025-03-07 12:21:44 +0200

    commit

---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class PrjClasses {
+
+    public static void main(String[] args) {
+        newClass nc = new newClass();// creating an object of newClass
+        nc.helloClass();// have to use variable name nc to communicate with he class
+        
+        System.out.println(nc.strSecondClassVar);
+        
+    //  System.out.println("Hello World!, I just did my first github activity and hate reece now.:)");
+    }
+}
